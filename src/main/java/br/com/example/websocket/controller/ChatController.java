e47e0ef29ea4de5aa83e95273ff1b826fc6b53d8
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2025-03-08 16:11:32 -0300

    init project

---
+++ src/main/java/br/com/example/websocket/controller/ChatController.java
@@ -0,0 +1,16 @@
+package br.com.example.websocket.controller;
+
+import br.com.example.websocket.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
---
+++ src/main/java/br/com/example/websocket/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package br.com.example.websocket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+    private String sender;
+    private String content;
+
+}
